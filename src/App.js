769592a149c12fdbad3d4362874da772a3adc411
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-20 19:10:53 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
 
 function App() {
 
   return (
     <div className="App">
      <Header/>
+     <Intro/>
     </div>
   );
 }
