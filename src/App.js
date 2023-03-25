8655cca1970900ff23f2e4c579b9dccfb34d5b6b
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-25 10:49:26 +0530

    experience section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './App.css';
-import Intro from './components/Intro/Intro';
+import React from "react";
+import "./App.css";
+import Intro from "./components/Intro/Intro";
 // import Faculty from './components/Faculty/Faculty';
-import BestEducation from './components/BestEducation/BestEducation';
+import BestEducation from "./components/BestEducation/BestEducation";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
 
 function App() {
-
   return (
     <div className="App">
-     <Intro/>
-     <BestEducation/>
-     {/* <Faculty/> */}
+      <Intro />
+      <BestEducation />
+      <Experience/>
+      <About />
+      {/* <Faculty/> */}
     </div>
   );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Experience.scss";
+
+const Experience = () => {
+  return (
+    <div className="Experience">
+      <div className="achievement">
+        <div className="circle">8+</div>
+        <span>years</span>
+        <span>Completed</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">500+</div>
+        <span>students</span>
+        <span>We Have</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">50+</div>
+        <span>teachers</span>
+        <span>In School</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
